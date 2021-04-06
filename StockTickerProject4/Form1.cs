2d6848bda73cacf8022a4568435546d7f6e83af4
Mohammad Zaidi <77636535+Mzaidiwb@users.noreply.github.com>
2021-04-06 19:58:52 -0400

    Add project files.

---
+++ StockTickerProject4/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace StockTickerProject4
+{
+    public partial class Form1 : Form
+    {
+        double x;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Tick += timer1_Tick;
+            timer1.Interval = 50;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                timer1.Start();
+            
+        }
+
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            List<string> MSFT1 = Program.CSV.MSFT_Price();
+            List<string> MSFT2 = Program.CSV.MSFT_Dates();
+
+
+            //chart1.Series[0].Points.AddXY(x, 3+ Math.Sin(x + 3));
+            chart1.Series[0].Points.DataBindXY(MSFT2,MSFT1);
+
+            if (chart1.Series[0].Points.Count > 100)
+                chart1.Series[0].Points.RemoveAt(0);
+
+            //chart1.ChartAreas[0].AxisX.Minimum = chart1.Series[0].Points[0].XValue;
+            //chart1.ChartAreas[0].AxisX.Maximum = x;
+
+            //x += 0.1;
+
+        }
+
+
+    }
+}
---
+++ StockTickerProject4/ReadCSV.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StockTickerProject4
+{
+    public class ReadCSV
+    {
+        public List<String> MSFT_Price()
+        {
+            using (var reader = new StreamReader(@"C:\Users\Mohammad\Downloads\MSFT.csv"))
+            {
+                List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    listB.Add(values[1]); //stock open
+                   
+                }
+                return listB;
+            }
+        }
+        public List<String> MSFT_Dates()
+        {
+            using (var reader = new StreamReader(@"C:\Users\Mohammad\Downloads\MSFT.csv"))
+            {
+                List<string> listA = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    listA.Add(values[0]); //dates
+
+
+                }
+                return listA;
+            }
+        }
+
+    }
+}
