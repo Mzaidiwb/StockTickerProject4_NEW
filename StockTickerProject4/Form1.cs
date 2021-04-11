a8463b95f9a44be793a35ebed44cf7a5ed405522
Mohammad Zaidi <77636535+Mzaidiwb@users.noreply.github.com>
2021-04-11 00:18:29 -0400

    Minor

--- StockTickerProject4/Form1.cs
+++ StockTickerProject4/Form1.cs
@@ -19,7 +19,6 @@ namespace StockTickerProject4
         public Form1()
         {
             InitializeComponent();
-
             chart1.ChartAreas[0].AxisY.IsStartedFromZero = false;
             chart2.ChartAreas[0].AxisY.IsStartedFromZero = false;
             chart3.ChartAreas[0].AxisY.IsStartedFromZero = false;
@@ -28,21 +27,6 @@ namespace StockTickerProject4
             chart6.ChartAreas[0].AxisY.IsStartedFromZero = false;
         }
 
-        public void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-           timer1.Start();
-           timer1.Tick += timer1_Tick;
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            timer1.Stop();
-        }
 
         public async void timer1_Tick(object sender, EventArgs e)
         {
@@ -64,50 +48,63 @@ namespace StockTickerProject4
             List<double> DIS_Price = DIS1.Select(double.Parse).ToList();
             List<double> KO_Price = KO1.Select(double.Parse).ToList();
 
-
-
-
             for (int i = 0; i < MS_PRICE.Count; i++)
             {
 
-                chart1.Series[0].Points.AddXY(MSFT2[i],MS_PRICE[i]);
-                if (chart1.Series[0].Points.Count > 100)
+                chart1.Series[0].Points.AddXY(MSFT2[i], MS_PRICE[i]);
+                if (chart1.Series[0].Points.Count > 20)
                     chart1.Series[0].Points.RemoveAt(0);
                 chart1.ChartAreas[0].RecalculateAxesScale();
 
                 chart2.Series[0].Points.AddXY(MSFT2[i], FB_Price[i]);
-                if (chart2.Series[0].Points.Count > 100)
+                if (chart2.Series[0].Points.Count > 20)
                     chart2.Series[0].Points.RemoveAt(0);
                 chart2.ChartAreas[0].RecalculateAxesScale();
 
                 chart3.Series[0].Points.AddXY(MSFT2[i], VOO_Price[i]);
-                if (chart3.Series[0].Points.Count > 100)
+                if (chart3.Series[0].Points.Count > 20)
                     chart3.Series[0].Points.RemoveAt(0);
                 chart3.ChartAreas[0].RecalculateAxesScale();
 
                 chart4.Series[0].Points.AddXY(MSFT2[i], F_Price[i]);
-                if (chart4.Series[0].Points.Count > 100)
+                if (chart4.Series[0].Points.Count > 20)
                     chart4.Series[0].Points.RemoveAt(0);
                 chart4.ChartAreas[0].RecalculateAxesScale();
 
                 chart5.Series[0].Points.AddXY(MSFT2[i], DIS_Price[i]);
-                if (chart5.Series[0].Points.Count > 100)
+                if (chart5.Series[0].Points.Count > 20)
                     chart5.Series[0].Points.RemoveAt(0);
                 chart5.ChartAreas[0].RecalculateAxesScale();
 
                 chart6.Series[0].Points.AddXY(MSFT2[i], KO_Price[i]);
-                if (chart6.Series[0].Points.Count > 100)
+                if (chart6.Series[0].Points.Count > 20)
                     chart6.Series[0].Points.RemoveAt(0);
                 chart6.ChartAreas[0].RecalculateAxesScale();
 
                 await Task.Delay(1000);
             }
-
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Tick += timer1_Tick;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer1.Interval = 500;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
     }
 }
